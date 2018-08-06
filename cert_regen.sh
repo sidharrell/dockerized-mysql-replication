3092b5d0a0de627c058084d3a4b69e80cf3ce827
sidney <sidney.harrell@gmail.com>
2018-08-06 10:35:07 -0400

    bug fix

--- cert_regen.sh
+++ cert_regen.sh
@@ -3,12 +3,17 @@ mkdir certs
 echo "These questions relate to the generation of the key."
 echo "You must enter a passphrase, you cannot skip, you cannot escape."
 echo "You have been warned!"
+echo
 openssl genrsa -des3 -out certs/myCA.key 2048
+echo
 echo "These questions relate to the CA you are creating."
+echo
 openssl req -x509 -new -nodes -key certs/myCA.key -sha256 -days 1825 -out certs/myCA.pem
 openssl genrsa -out selfsigned.key 2048
+echo
 echo "These questions relate to the CSR generation."
-openssl req -new -key selfsigned.key -out selfsigned.csr
+echo
+openssl req -new -key certs/selfsigned.key -out certs/selfsigned.csr
 cat <<EOF > certs/selfsigned.ext
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
@@ -17,10 +22,14 @@ subjectAltName = @alt_names
 
 [alt_names]
 EOF
+echo
 read -p "What is the hostname you want to use for the first host? " -r
 echo $REPLY >> certs/selfsigned.ext
 read -p "What is the hostname you want to use for the second host? " -r
 echo $REPLY >> certs/selfsigned.ext
+echo
+echo "Signing"
+echo
 openssl x509 -req -in certs/selfsigned.csr -CA certs/myCA.pem -CAkey certs/myCA.key -CAcreateserial -out certs/selfsigned.crt -days 1825 -sha256 -extfile certs/selfsigned.ext
 cat certs/selfsigned.key certs/selfsigned.crt > certs/selfsigned.pem
 cp certs/selfsigned.pem roles/haproxy/files/selfsigned.pem
